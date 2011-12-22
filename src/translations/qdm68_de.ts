be3cf8cc9651ac8ec1ff5ff1fba0a5f27c35c437
sOuSiX <sousix@defrag.fr>
2011-12-22 16:45:22 +0100

    Add german translation (incomplete) Fix a word in french translation

--- src/translations/qdm68_de.ts
+++ src/translations/qdm68_de.ts
@@ -1,52 +1,52 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="fr_FR">
+<TS version="2.0" language="de_DE">
 <context>
     <name>MainWindow</name>
     <message>
         <location filename="../mainwindow.ui" line="54"/>
         <source>Search...</source>
-        <translation>Rechercher...</translation>
+        <translation>Suche...</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="485"/>
         <source>Slowest</source>
-        <translation>+ lentes</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="488"/>
         <source>Select slowest demos</source>
-        <translation>Selectionner les demos les plus lentes</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="497"/>
         <source>Fastest</source>
-        <translation>+ rapides</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="500"/>
         <source>Select best demos</source>
-        <translation>Selectionner les demos les plus rapides</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="509"/>
         <source>Invert</source>
-        <translation>Inverser</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="533"/>
         <source>More details</source>
-        <translation>Plus de détails sur les démos</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="542"/>
         <source>Folder</source>
-        <translation>Dossier</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="41"/>
         <source>Play</source>
-        <translation>Jouer</translation>
+        <translation>Abspielen</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="152"/>
@@ -57,219 +57,219 @@
     <message>
         <location filename="../mainwindow.ui" line="521"/>
         <source>Unselect all</source>
-        <translation>Tout décocher</translation>
+        <translation>Alles deselektieren</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="512"/>
         <source>Invert selection</source>
-        <translation>Inverser</translation>
+        <translation>Auswahl invertieren</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="103"/>
         <source>Move to...</source>
-        <translation>Déplacer...</translation>
+        <translation>Verschieben nach...</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="113"/>
         <source>Copy to...</source>
-        <translation>Copier...</translation>
+        <translation>Kopieren nach...</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="123"/>
         <source>Delete</source>
-        <translation>Supprimer</translation>
+        <translation>Löschen</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="369"/>
         <source>File</source>
-        <translation>Fichier</translation>
+        <translation>Datei</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="378"/>
         <location filename="../mainwindow.ui" line="417"/>
         <source>Options</source>
-        <translation>Options</translation>
+        <translation>Optionen</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="387"/>
         <source>Help</source>
-        <translation>Aide</translation>
+        <translation>Hilfe</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="422"/>
         <source>Quit</source>
-        <translation>Quitter</translation>
+        <translation>Beenden</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="431"/>
         <source>Demos folder...</source>
-        <translation>Dossier de démos...</translation>
+        <translation>Verzeichnis der Demos...</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="443"/>
         <source>Statistics</source>
-        <translation>Statistiques</translation>
+        <translation>Statistiken</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="455"/>
         <source>Settings</source>
-        <translation>Configuration</translation>
+        <translation>Einstellungen</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="463"/>
         <location filename="../mainwindow.cpp" line="560"/>
         <source>About QDM68</source>
-        <translation>A propos de QDM68</translation>
+        <translation>Über QDM68</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="468"/>
         <source>About Qt</source>
-        <translation></translation>
+        <translation>Über Qt</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="473"/>
         <source>Analyse all demos</source>
-        <translation>Analyser toutes les démos</translation>
+        <translation>Alle Demos analysieren</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="476"/>
         <source>For faster reading of demos informations</source>
-        <translation></translation>
+        <translation type="unfinished">For faster reading of demos informations</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="280"/>
         <location filename="../mainwindow.cpp" line="311"/>
         <source>Select folder</source>
-        <translation>Selectionner un dossier</translation>
+        <translation>Verzeichnis selektieren</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="286"/>
         <source>Copying files...</source>
-        <translation>Copies en cours...</translation>
+        <translation>Kopiere Dateien...</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="286"/>
         <source>Abort Copy</source>
-        <translation>Annuler la copie</translation>
+        <translation>Kopieren abbrechen</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="337"/>
         <location filename="../mainwindow.cpp" line="374"/>
         <source>Warning</source>
-        <translation>Attention</translation>
+        <translation>Warnung</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="337"/>
         <source>One or more files could not be moved.</source>
-        <translation>Un ou plusieurs fichiers n&apos;ont pas été déplacés.</translation>
+        <translation>Eine oder mehrere Dateien konnten nicht verschoben werden.</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="231"/>
         <source>No slowest demos found!</source>
-        <translation>Aucune démo plus lente trouvée!</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="346"/>
         <source>Information</source>
-        <translation>Information</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="347"/>
         <source>Are you sure you want to delete %1 files ?</source>
-        <translation>Êtes vous sûr de vouloir supprimer %1 fichiers ?</translation>
+        <translation>Wollen Sie wirklich %1 Dateien löschen?</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="349"/>
         <source>Yes</source>
-        <translation>Oui</translation>
+        <translation>Ja</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="350"/>
         <source>Cancel</source>
-        <translation>Annuler</translation>
+        <translation>Abbrecvhen</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="374"/>
         <source>One or more files could not be deleted.</source>
-        <translation>Un ou plusieurs fichiers n&apos;ont pas pu être supprimés.</translation>
+        <translation>Eine oder mehrere Dateien konnten nicht gelöscht werden.</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="404"/>
         <source>%1 demo(s) selected</source>
-        <translation>%1 démo(s) sélectionnée(s)</translation>
+        <translation>%1 Démos selektiert</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="545"/>
         <location filename="../mainwindow.cpp" line="412"/>
         <source>Select demos folder</source>
-        <translation>Selectionnez un dossier de démos</translation>
+        <translation>Demoverzeichnis selektieren</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="499"/>
         <location filename="../mainwindow.cpp" line="573"/>
         <source>%1 demo(s) found</source>
         <oldsource>%1 demos found</oldsource>
-        <translation>%1 demo(s) trouvée(s)</translation>
+        <translation>%1 Demo(s) gefunden</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="511"/>
         <source>Analyzing... %1/%2</source>
-        <translation>Analyse... %1/%2</translation>
+        <translation>Analysiere... %1/%2</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="553"/>
         <source>Author</source>
-        <translation>Auteur</translation>
+        <translation>Autor</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="554"/>
         <source>Thanks to</source>
-        <translation>Merci à</translation>
+        <translation>Danke an</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="556"/>
         <source>Created with</source>
-        <translation>Créé avec</translation>
+        <translation>Erstellt mit</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="557"/>
         <source>This software use </source>
-        <translation>Ce logiciel utilise</translation>
+        <translation>Diese Software verwendet</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="558"/>
         <source>Checkout sources at</source>
-        <translation>Sources disponibles sur</translation>
+        <translation type="unfinished">Checkout sources at</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="626"/>
         <source>Unavailable</source>
-        <translation>Indisponible</translation>
+        <translation>Nicht vorhanden</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="642"/>
         <source>No demo selected</source>
-        <translation>Aucune démo sélectionnée</translation>
+        <translation>Keine Demo selektiert</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="691"/>
         <source>No demos selected</source>
-        <translation>Aucune démo selectionnée</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="701"/>
         <source>Quake 3 engine not found</source>
-        <translation>L&apos;executable de Quake 3 n&apos;a pas été trouvé</translation>
+        <translation>Quake 3 Engine nicht gefunden.</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="718"/>
         <source>Executing &apos;%1&apos;</source>
-        <translation>Execution de &apos;%1&apos;</translation>
+        <translation>Starte &apos;%1&apos;</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="991"/>
         <source>Settings could not be saved.</source>
-        <translation>La configuration ne peut être sauvegardée.</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -277,47 +277,47 @@
     <message>
         <location filename="../settingsdialog.ui" line="14"/>
         <source>Settings</source>
-        <translation>Configuration</translation>
+        <translation>Einstellungen</translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="20"/>
         <source>Demo player</source>
-        <translation>Lecteur de démos</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="31"/>
         <source>Quake 3 engine :</source>
-        <translation>Executable Quake 3 :</translation>
+        <translation>Quake 3 Engine:</translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="131"/>
         <source>Selection</source>
-        <translation>Selection</translation>
+        <translation>Auswahl</translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="137"/>
         <source>Distinct players when selecting slowest or fastest demos</source>
-        <translation>Distinguer les joueurs lors de la selections des démos les plus lentes ou plus rapides</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="59"/>
         <source>Rules</source>
-        <translation>Règles</translation>
+        <translation type="unfinished">Regeln</translation>
     </message>
     <message>
         <location filename="../settingsdialog.cpp" line="58"/>
         <source>Select quake 3 engine</source>
-        <translation>Selection du moteur de quake 3</translation>
+        <translation>Auswahl der Quake 3 Engine </translation>
     </message>
     <message>
         <location filename="../settingsdialog.cpp" line="93"/>
         <source>Variable</source>
-        <translation>Variable</translation>
+        <translation type="unfinished">Schlüssel</translation>
     </message>
     <message>
         <location filename="../settingsdialog.cpp" line="94"/>
         <source>Value</source>
-        <translation>Valeur</translation>
+        <translation>Wert</translation>
     </message>
 </context>
 <context>
@@ -325,37 +325,37 @@
     <message>
         <location filename="../statisticsdialog.ui" line="17"/>
         <source>Statistics</source>
-        <translation>Statistiques</translation>
+        <translation>Statistiken</translation>
     </message>
     <message>
         <location filename="../statisticsdialog.ui" line="36"/>
         <source>Total :</source>
-        <translation>Total :</translation>
+        <translation>Demos insgesamt:</translation>
     </message>
     <message>
         <location filename="../statisticsdialog.ui" line="76"/>
         <source>Offline :</source>
-        <translation></translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../statisticsdialog.ui" line="137"/>
         <source>Online :</source>
-        <translation></translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../statisticsdialog.ui" line="195"/>
         <source>Time :</source>
-        <translation>Temps :</translation>
+        <translation>Zeit:</translation>
     </message>
     <message>
         <location filename="../statisticsdialog.ui" line="397"/>
         <source>Note : statistics concerns only the current serach results.</source>
-        <translation>Note :  les statistiques concernent uniquement les résultats de la recherche.</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../statisticsdialog.ui" line="422"/>
         <source>Close</source>
-        <translation>Fermer</translation>
+        <translation>Schließen</translation>
     </message>
 </context>
 </TS>
